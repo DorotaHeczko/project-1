31201606511f6c9b197b23f2cda4dbb0566f61b1
Dorota Heczko <117163329+DorotaHeczko@users.noreply.github.com>
2023-04-02 10:58:49 +0200

    project

---
+++ index.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes */
+const getBtn = document.querySelector("button");
+let number = 1;
+const addEl = function () {
+  const newBox = document.createElement("div");
+  newBox.classList.add("box");
+  newBox.textContent = number;
+  number++;
+
+  if (number % 5 == 0) {
+    newBox.classList.add("circle");
+  }
+  document.body.appendChild(newBox);
+};
+
+getBtn.addEventListener("click", addEl);
